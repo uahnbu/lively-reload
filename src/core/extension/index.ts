1592cfd33e044a88a98bf63902fbd8d6a71ee180
uahnbu <uahnbu29main@gmail.com>
2021-08-23 17:13:14 +0700

    🎨 Change tabs to spaces

--- src/core/extension/index.ts
+++ src/core/extension/index.ts
@@ -5,34 +5,34 @@ import { loadStatusButton } from './utils/statusButton';
 import type { ExtensionContext } from 'vscode';
 
 export {
-	showMessage,
-	getRoot,
-	getPacker,
-	getActiveHtmlData,
-	getConfig,
-	openBrowser,
-	focusContent
+  showMessage,
+  getRoot,
+  getPacker,
+  getActiveHtmlData,
+  getConfig,
+  openBrowser,
+  focusContent
 } from './utils/commands';
 
 export const START_COMMAND = 'livelyReload.startLively';
 export const CLOSE_COMMAND = 'livelyReload.closeLively';
 
 export {
-	setStatusButtonDoStart,
-	setStatusButtonLoading,
-	setStatusButtonDoClose
+  setStatusButtonDoStart,
+  setStatusButtonLoading,
+  setStatusButtonDoClose
 } from './utils/statusButton';
 
 const RELOAD_COMMAND = 'livelyReload.reloadLively';
 
 export function activate(context: ExtensionContext) {
-	loadStatusButton();
-	context.subscriptions.push(...[
-		commands.registerCommand(START_COMMAND, () => startServer()),
-		commands.registerCommand(CLOSE_COMMAND, () => closeServer()),
-		commands.registerCommand(RELOAD_COMMAND, () => reloadServer())
-	]);
-	listenEditorEvents();
+  loadStatusButton();
+  context.subscriptions.push(...[
+    commands.registerCommand(START_COMMAND, () => startServer()),
+    commands.registerCommand(CLOSE_COMMAND, () => closeServer()),
+    commands.registerCommand(RELOAD_COMMAND, () => reloadServer())
+  ]);
+  listenEditorEvents();
 }
 
 `.        ╭───────╮
